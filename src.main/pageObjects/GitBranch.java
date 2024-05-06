1f609e8420e0c14f5c7d1ec3ce89a7a510aada0f
”rakeshparayil” <rakeshpparayil@gmail.com>
2024-05-06 16:51:32 +1000

    Archtect American development

---
+++ src.main/pageObjects/GitBranch.java
@@ -0,0 +1,22 @@
+package pageObjects;
+
+import org.testng.annotations.Test;
+
+public class GitBranch {
+	@Test
+	public void Appium()
+	{
+		System.out.println("postAppium1");
+		System.out.println("postAppium2");
+
+	}
+	
+	@Test
+	public void RestAssured()
+	{
+		System.out.println("RestAssured1");
+		System.out.println("RestAssured2");
+
+	}
+
+}
--- src.main/pageObjects/GitTest.java
+++ src.main/pageObjects/GitTest.java
@@ -15,5 +15,26 @@ public class GitTest {
 		System.out.println("postJira5");
 		System.out.println("postJira6");
 	}
+	
+	@Test
+	public void Csharp()
+	{
+		System.out.println("Csharp1");
+		System.out.println("Csharp2");
+		System.out.println("Csharp3");
+		
+		
+	}
+	
+	@Test
+	public void Java()
+	{
+		System.out.println("Java1");
+		System.out.println("Java2");
+		System.out.println("Java3");
+		
+		
+	}
+
 
 }
