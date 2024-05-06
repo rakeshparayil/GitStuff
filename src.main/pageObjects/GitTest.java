bee38c87fd8c0233e3e6c3a3075b547ef704a7b2
”rakeshparayil” <rakeshpparayil@gmail.com>
2024-05-06 12:43:22 +1000

    commit from Asian time zone

---
+++ src.main/pageObjects/GitTest.java
@@ -0,0 +1,15 @@
+package pageObjects;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	@Test
+	public void PostJira()
+	{
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	}
+
+}
